93962c2fd9089a46272221d5904ebeae9d9c846d
Juan Francisco <contrerasjf.0@hotmail.com>
2021-11-04 06:54:25 -0600

    update(Products): Implement the product service in the product controller
    Resolve: C-15

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,4 +1,55 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  Param,
+  Post,
+  Body,
+  Put,
+  Delete,
+  HttpStatus,
+  HttpCode,
+  Res,
+} from '@nestjs/common';
+
+import { ProductsService } from './../services/products.service';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private productsService: ProductsService) {}
+
+  @Get()
+  getProducts(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    return this.productsService.findAll();
+  }
+
+  @Get('filter')
+  getProductFilter() {
+    return `yo soy un filter`;
+  }
+
+  @Get(':productId')
+  @HttpCode(HttpStatus.ACCEPTED)
+  getOne(@Param('productId') productId: string) {
+    return this.productsService.findOne(+productId);
+  }
+
+  @Post()
+  create(@Body() payload: any) {
+    return this.productsService.create(payload);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() payload: any) {
+    return this.productsService.update(+id, payload);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return id;
+  }
+}
