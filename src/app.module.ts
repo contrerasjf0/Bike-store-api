534c055044154ce8e0775685838a6837f9a2938a
Juan Francisco <contrerasjf.0@hotmail.com>
2021-11-05 07:02:28 -0600

    features(): Implement the script for others entities
    * The entities and dtos were implemented for brands, user, customers and categoties.
    * The controllers  and services were implemented for brands, user, customers and categoties.
    
    Resolve: CU0-C-22

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,17 @@ import { AppService } from './app.service';
 import { CategoriesController } from './controllers/categories.controller';
 import { ProductsController } from './controllers/products.controller';
 import { ProductsService } from './services/products.service';
+import { BrandsController } from './controllers/brands.controller';
+import { CustomersController } from './controller/customers.controller';
+import { UsersController } from './controller/users.controller';
+import { BrandsService } from './services/brands.service';
+import { CategoriesService } from './services/categories.service';
+import { CustomersService } from './services/customers.service';
+import { UsersService } from './services/users.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, CategoriesController, ProductsController],
-  providers: [AppService, ProductsService],
+  controllers: [AppController, CategoriesController, ProductsController, BrandsController, CustomersController, UsersController],
+  providers: [AppService, ProductsService, BrandsService, CategoriesService, CustomersService, UsersService],
 })
 export class AppModule {}
