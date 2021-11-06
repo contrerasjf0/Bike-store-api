efb1d6749d29b2c3e92ae56fc41561a7f02b21bd
Juan Francisco <contrerasjf.0@hotmail.com>
2021-11-06 06:16:37 -0600

    refactor(Entities): Refactor the code to modules
    * The brand, cproducts and categories entities with their scripts were moved to the product  module.
    * The user and costumer entities withs their scripts were moved to the users module.
    
    Resolve: CU1-C-3

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,13 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CategoriesController } from './controllers/categories.controller';
-import { ProductsController } from './controllers/products.controller';
-import { ProductsService } from './services/products.service';
-import { BrandsController } from './controllers/brands.controller';
-import { CustomersController } from './controller/customers.controller';
-import { UsersController } from './controller/users.controller';
-import { BrandsService } from './services/brands.service';
-import { CategoriesService } from './services/categories.service';
-import { CustomersService } from './services/customers.service';
-import { UsersService } from './services/users.service';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CategoriesController, ProductsController, BrandsController, CustomersController, UsersController],
-  providers: [AppService, ProductsService, BrandsService, CategoriesService, CustomersService, UsersService],
+  imports: [UsersModule, ProductsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
