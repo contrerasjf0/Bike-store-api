9a577bb956784af893930b98a8738c5b276b1998
Juan Francisco <contrerasjf.0@hotmail.com>
2021-11-06 19:21:05 -0600

    config(enviroment): Implement schema validation with Joi
    * The Joi library was installed.
    * The config schema validation was defined in the app.module file
    
    Resolve: CU1-C-12

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module, HttpModule, HttpService } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-
+import * as Joi from 'joi';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -19,6 +19,11 @@ const API_KEY_PROD = 'PROD1212121SA';
       envFilePath: enviroments[process.env.NODE_ENV] || '.env',
       load: [config],
       isGlobal: true,
+      validationSchema: Joi.object({ // 👈
+        API_KEY: Joi.number().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+      }),
     }),
     HttpModule,
     UsersModule,
