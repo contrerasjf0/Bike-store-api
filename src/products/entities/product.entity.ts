ae3c850d267b00560c6af96fce56445cc417fc76
Juan Francisco <contrerasjf.0@hotmail.com>
2021-11-09 19:39:02 -0600

    update(product): Define new fields to handle Creations dates
    Resolve: CU2-C-17

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,9 @@
-import { PrimaryGeneratedColumn, Column, Entity } from 'typeorm';
+import {  
+  PrimaryGeneratedColumn,
+  Column,
+  Entity,
+  CreateDateColumn,
+  UpdateDateColumn, } from 'typeorm';
 
 @Entity()
 export class Product {
@@ -19,4 +24,17 @@ export class Product {
 
   @Column({ type: 'varchar' })
   image: string;
+
+  @CreateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updateAt: Date;
+
 }
